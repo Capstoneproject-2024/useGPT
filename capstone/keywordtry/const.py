9a14ce94d12a5624146f5db9fdc5003c69229500
DESKTOP-O9A3R98\jeong <jeonginlee72@gmail.com>
2024-09-22 01:37:55 +0900

    feat : review keyword 미완성

--- capstone/keywordtry/const.py
+++ capstone/keywordtry/const.py
@@ -1,20 +1,20 @@
-prompt_always_keywords = """
-You must answer not sentence, just keywords in Korean language for your responses.
+prompt_always_kor = """
+You must answer not sentence, just keywords in Korean language for your responses. You have to use only Korean.
 """
 
 # 아직 작가 이름 못둘러쌈..
 publisher_keyword_extract_prompt = """
 You are a word-renowned book critic.
- Your task is to extract 5 keywords from a text which represents introductions and summary of a book written by publisher.
+Your task is to extract 5 keywords from a text which represents introductions and summary of a book written by publisher.
 If the text is incomplete or cut off, infer the likely conclusion based on the available context.
 5 keywords must be similar to the subject of book and must be clear to know the content of the book.
 
 Here’s how you should extract the keywords:
 1. you have to translate title and text to English.
-2. extract specific and detailed keywords that reflect the themes or topics of the book description.
+2. Extract specific and detailed keywords that reflect the themes or topics of the book description.
     It shouldn't be just single words like 'passion' or 'mystery.'.
     Keywords should be more descriptive phrases like 'passion for trends,' 'investigating a murder,' or 'modern female solidarity.'
-3. determine the ranking of keywords by correlation with text and show it in order.
+3. Determine the ranking of keywords by correlation with text and show it in order.
 4. Whenever your keywords come across with character names or fictional places that are unique to the book, enclose them in single quotation marks ('') to distinguish them.
 
 
@@ -22,5 +22,27 @@ This is precaution what you should be careful for:
 - Do not include external details like the author's name, awards, or any information unrelated to the book's subject matter.
 
 The book title is {{title}} and here is the text.
+{{content}}
+"""
+
+review_keyword_extract_prompt = """
+You are a word-renowned text analyst.
+Your task is to analyze book reviews and make 3 keywords from a book review written by a normal person.
+Extract up to 3 keywords that capture the most significant themes or emotions from the review.
+
+Here’s how you should extract the keywords:
+1. You have to translate title and review to English.
+2. Analyze the book reviews to identify the specific points and themes where the reviewer felt the most emotionally moved.
+    Then extract or make keywords that capture the most significant themes or emotions from the review.
+    It shouldn't be just single words like 'passion' or 'mystery.'.
+    Keywords should be more descriptive phrases like 'passion for trends,' 'investigating a murder,' or 'modern female solidarity.'
+3. determine the ranking of keywords by correlation with the review and show it in order.
+
+
+Keypoint of this work is to focus on capturing the depth of their emotions and express it in detailed and nuanced language, rather than merely summarizing the content.
+If the review is too short, it is okay to provide keywords less than 3.
+Quality of keywords is more important than Number of keywords. 
+
+The book title is {{title}} and here is the review.
 {{content}}
 """
--- capstone/keywordtry/try.py
+++ capstone/keywordtry/try.py
@@ -6,6 +6,7 @@ import asyncio
 from textEX import textList
 from const import *
 from utils import inject_variables
+from reviewEX import reviewList
 
 load_dotenv()
 
@@ -16,13 +17,14 @@ OPENAI_MODEL = "gpt-4o-mini"
 client = AsyncOpenAI(api_key=GPT_API_KEY)
 
 
-async def extract(client):
+# 출판사 키워드 추출
+async def extractPublisher(client):
     for i in range(len(textList)):
         prompt = inject_variables(publisher_keyword_extract_prompt, textList[i])
         response = await client.chat.completions.create(
             model=OPENAI_MODEL,
             messages=[
-                {"role": "system", "content": prompt + prompt_always_keywords},
+                {"role": "system", "content": prompt + prompt_always_kor},
             ],
             temperature=1.3,
             top_p=0.8,
@@ -35,8 +37,29 @@ async def extract(client):
         print(temp["choices"][0]["message"]["content"] + "\n")
 
 
+# 리뷰 키워드 추출
+async def extractReview(client):
+    for i in range(len(reviewList)):
+        prompt = inject_variables(review_keyword_extract_prompt, reviewList[i])
+        response = await client.chat.completions.create(
+            model=OPENAI_MODEL,
+            messages=[
+                {"role": "system", "content": prompt + prompt_always_kor},
+            ],
+            temperature=1.3,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=1,
+        )
+
+        # print(response)
+        temp = response.model_dump()
+        print(str(i) + "\n" + temp["choices"][0]["message"]["content"] + "\n")
+
+
 async def main():
-    await extract(client=client)
+    # await extractPublisher(client=client)
+    await extractReview(client=client)
 
 
 # main 함수 실행
